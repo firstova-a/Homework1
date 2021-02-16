5b5427220370f15e2815d4bd391d66f77de6f741
firstova-a <firstova-a@yandex.ru>
2021-02-16 22:22:44 +0300

    Создано консольное приложение, которое выводит текущие день недели и время

---
+++ Time/Time/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime pr = DateTime.Now;
+            Console.WriteLine($"Текущий день недели: {(int) pr.DayOfWeek} ({pr.DayOfWeek})");
+            Console.WriteLine($"Текущее время: {pr:HH:mm:ss}");
+            Console.ReadLine();
+        }
+    }
+}
